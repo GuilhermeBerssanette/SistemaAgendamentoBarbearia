e108e13050d27a0ec5ba26257f1ef354cb1f978e
Guilherme Berssanette <guilherme.berssanette@gmail.com>
2024-07-27 18:07:19 -0300

    fix for firebase connection

---
+++ src/environments/environment.development.ts
@@ -0,0 +1,11 @@
+export const environment = {
+    firebaseConfig: {
+      apiKey: "AIzaSyA7EAictaRyiO4QBJc7nL347oBK3oa0vn4",
+      authDomain: "sistemaagendamento-774d4.firebaseapp.com",
+      projectId: "sistemaagendamento-774d4",
+      storageBucket: "sistemaagendamento-774d4.appspot.com",
+      messagingSenderId: "592870740609",
+      appId: "1:592870740609:web:7e1efc1b095ae87e03f380",
+      measurementId: "G-TPS6D23B6C"
+    }
+  };
