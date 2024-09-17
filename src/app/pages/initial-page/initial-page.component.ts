5c9a45ef2570f4262663c281a85e3a53266b3b25
Guilherme Berssanette <guilherme.berssanette@gmail.com>
2024-09-17 18:23:27 -0300

    Generate card barbershop

--- src/app/pages/initial-page/initial-page.component.ts
+++ src/app/pages/initial-page/initial-page.component.ts
@@ -1,8 +1,9 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {ModalFilterComponent} from "../modals/modal-filter/modal-filter.component";
 import { MatDialog } from '@angular/material/dialog';
 import {RouterLink} from "@angular/router";
-import {NgOptimizedImage} from "@angular/common";
+import {NgForOf, NgOptimizedImage} from "@angular/common";
+import {BarbeariasService} from "../../services/barbearias.service";
 
 
 @Component({
@@ -10,15 +11,23 @@ import {NgOptimizedImage} from "@angular/common";
   standalone: true,
   imports: [
     RouterLink,
-    NgOptimizedImage
+    NgOptimizedImage,
+    NgForOf
   ],
   templateUrl: './initial-page.component.html',
   styleUrl: './initial-page.component.scss'
 })
-export class InitialPageComponent {
+export class InitialPageComponent implements OnInit{
 
-  constructor(public dialog: MatDialog) {
+  barbearias: any[] = [];
 
+  constructor(public dialog: MatDialog,
+              private barbeariasService: BarbeariasService) {
+
+  }
+
+  async ngOnInit() {
+    this.barbearias = await this.barbeariasService.getBarbearias();
   }
 
   openModalFilter() {
