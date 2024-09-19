32e65c6cebd020e6ae38de8b1971d77667f0e91a
Guilherme Berssanette <guilherme.berssanette@gmail.com>
2024-09-19 20:33:32 -0300

    connecting the card to the barbershop page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,10 +3,12 @@ import { LoginComponent} from "./pages/login/login.component";
 import { RegisterComponent} from "./pages/register/register.component";
 import {InitialPageComponent} from "./pages/initial-page/initial-page.component";
 import {RegisterBarbeariaComponent} from "./pages/register-barbearia/register-barbearia.component";
+import {BarbershopComponent} from "./pages/barbershop/barbershop.component";
 
 export const routes: Routes = [
   {path: '', component: LoginComponent},
   {path: 'register', component: RegisterComponent},
   {path: 'initial-page', component: InitialPageComponent},
-  {path: 'register-barbearia', component: RegisterBarbeariaComponent}
+  {path: 'register-barbearia', component: RegisterBarbeariaComponent},
+  { path: 'barbearia/:id', component: BarbershopComponent },
 ];
