83d1daf3877b0e84d7fb2136ddfa7a4dcabfc7fb
Guilherme Berssanette <guilherme.berssanette@gmail.com>
2024-09-19 20:33:32 -0300

    connecting the card to the barbershop page

--- src/app/pages/initial-page/initial-page.component.ts
+++ src/app/pages/initial-page/initial-page.component.ts
@@ -1,7 +1,8 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, inject, OnInit} from '@angular/core';
 import {ModalFilterComponent} from "../modals/modal-filter/modal-filter.component";
 import { MatDialog } from '@angular/material/dialog';
-import {RouterLink} from "@angular/router";
+import {HttpClient} from "@angular/common/http";
+import {Router, RouterLink} from "@angular/router";
 import {NgForOf, NgOptimizedImage} from "@angular/common";
 import {BarbeariasService} from "../../services/barbearias.service";
 
@@ -10,24 +11,30 @@ import {BarbeariasService} from "../../services/barbearias.service";
   selector: 'app-initial-page',
   standalone: true,
   imports: [
-    RouterLink,
     NgOptimizedImage,
-    NgForOf
+    NgForOf,
+    RouterLink
   ],
   templateUrl: './initial-page.component.html',
   styleUrl: './initial-page.component.scss'
 })
 export class InitialPageComponent implements OnInit{
 
+  http = inject(HttpClient);
+  router = inject(Router)
+
   barbearias: any[] = [];
 
   constructor(public dialog: MatDialog,
               private barbeariasService: BarbeariasService) {
-
   }
 
   async ngOnInit() {
     this.barbearias = await this.barbeariasService.getBarbearias();
+  }
+
+  goToBarbearia(id: string) {
+    this.router.navigate(['/barbearia', id]).then();
   }
 
   openModalFilter() {
