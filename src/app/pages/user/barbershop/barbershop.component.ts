b2bbd24ddca315cb2effd60d041fc3006e33935b
Guilherme Berssanette <guilherme.berssanette@gmail.com>
2024-09-27 15:24:25 -0300

    create modal barber info and media link

--- src/app/pages/user/barbershop/barbershop.component.ts
+++ src/app/pages/user/barbershop/barbershop.component.ts
@@ -1,15 +1,16 @@
 import { Component, OnInit } from '@angular/core';
-import {ActivatedRoute, RouterLink} from '@angular/router';
-import {collection, doc, Firestore, getDoc, getDocs} from '@angular/fire/firestore';
-
+import { ActivatedRoute, RouterLink } from '@angular/router';
+import { collection, doc, Firestore, getDoc, getDocs } from '@angular/fire/firestore';
 import { CommonModule } from '@angular/common';
-import {Barbearias} from "../../../interfaces/barbearias";
-import {Barbeiros} from "../../../interfaces/barbeiros"; // importando o CommonModule
+import { Barbearias } from "../../../interfaces/barbearias";
+import { Barbeiros } from "../../../interfaces/barbeiros";
+import {ModalInfoComponent} from "./modals/modal-info/modal-info.component";
+import {MatDialog} from "@angular/material/dialog";
 
 @Component({
   selector: 'app-barbershop',
   standalone: true,
-  imports: [CommonModule, RouterLink, ], // garantindo que o CommonModule esteja importado
+  imports: [CommonModule, RouterLink],
   templateUrl: './barbershop.component.html',
   styleUrls: ['./barbershop.component.scss']
 })
@@ -19,7 +20,7 @@ export class BarbershopComponent implements OnInit {
   barbeariaId?: string;
   barbers: Barbeiros[] = [];
 
-  constructor(private route: ActivatedRoute, private firestore: Firestore) {}
+  constructor(private route: ActivatedRoute, private firestore: Firestore, public dialog: MatDialog) {}
 
   async ngOnInit() {
     this.barbeariaId = this.route.snapshot.paramMap.get('id')!;
@@ -45,6 +46,39 @@ export class BarbershopComponent implements OnInit {
         ...doc.data()
       })) as Barbeiros[];
     }
-
   }
+
+  isAddressValid(): boolean {
+    return !!(this.barbearia?.estado && this.barbearia?.cidade && this.barbearia?.rua);
+  }
+
+  openGoogleMaps(): void {
+    if (this.isAddressValid()) {
+      const { estado, cidade, rua, numero } = this.barbearia!;
+      const address = `${rua}, ${numero ? numero + ', ' : ''}${cidade}, ${estado}`;
+      const url = `https://www.google.com/maps/search/?api=1&query=${encodeURIComponent(address)}`;
+
+      try {
+        const newWindow = window.open(url, '_blank');
+
+        if (!newWindow || newWindow.closed || typeof newWindow.closed == 'undefined') {
+          alert('Endereço não encontrado. Verifique as informações fornecidas.');
+        }
+      } catch (error) {
+        alert('Erro ao abrir o Google Maps. Por favor, tente novamente.');
+      }
+    }
+  }
+
+  openModalInfos(barber: Barbeiros) {
+    const dialogRef = this.dialog.open(ModalInfoComponent, {
+      width: '300px',
+      data: barber
+    });
+
+    dialogRef.afterClosed().subscribe(result => {
+      console.log('O modal foi fechado');
+    });
+  }
+
 }
