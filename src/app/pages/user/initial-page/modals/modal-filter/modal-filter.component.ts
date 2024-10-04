0f0eca8c734944fba1cf242ca1e98606f859ae56
joaovs19 <joaovitorsa20@gmail.com>
2024-10-04 18:58:26 -0300

    files Initial Page and Modal Filter

--- src/app/pages/user/initial-page/modals/modal-filter/modal-filter.component.ts
+++ src/app/pages/user/initial-page/modals/modal-filter/modal-filter.component.ts
@@ -1,12 +1,20 @@
 import { Component } from '@angular/core';
+import { MatDialogRef } from '@angular/material/dialog';
+import {MatIconModule} from '@angular/material/icon';
 
 @Component({
   selector: 'app-modal-filter',
   standalone: true,
-  imports: [],
+  imports: [MatIconModule,],
   templateUrl: './modal-filter.component.html',
   styleUrl: './modal-filter.component.scss'
 })
 export class ModalFilterComponent {
+  [x: string]: any;
 
+    constructor(private dialogRef: MatDialogRef<ModalFilterComponent>){ }
+
+    closeModalFilter(){
+      this.dialogRef.close();
+    }
 }
