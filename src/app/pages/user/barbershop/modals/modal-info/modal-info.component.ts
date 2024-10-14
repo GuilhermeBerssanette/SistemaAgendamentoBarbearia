59dcbab8fa15769e1bbc0ab8c62a37d5428c1e0f
Guilherme Berssanette <guilherme.berssanette@gmail.com>
2024-10-14 18:27:21 -0300

    att modal info barber

--- src/app/pages/user/barbershop/modals/modal-info/modal-info.component.ts
+++ src/app/pages/user/barbershop/modals/modal-info/modal-info.component.ts
@@ -1,20 +1,54 @@
-import {Component, Inject} from '@angular/core';
-import {MAT_DIALOG_DATA, MatDialogRef} from "@angular/material/dialog";
-import {Barbeiros} from "../../../../../interfaces/barbeiros";
-import {MatButton} from "@angular/material/button";
+import { Component, Inject, OnInit } from '@angular/core';
+import { MAT_DIALOG_DATA, MatDialogRef } from "@angular/material/dialog";
+import { Firestore, collection, getDocs } from '@angular/fire/firestore';
+import { Barbeiros } from "../../../../../interfaces/barbeiros";
+import { NgIf, NgForOf, CurrencyPipe } from '@angular/common';
+
+interface Service {
+  id: string;
+  duration: number;
+  price: number;
+}
 
 @Component({
   selector: 'app-modal-info',
   standalone: true,
   imports: [
-    MatButton
+    NgIf,
+    NgForOf,
+    CurrencyPipe
   ],
   templateUrl: './modal-info.component.html',
-  styleUrl: './modal-info.component.scss'
+  styleUrls: ['./modal-info.component.scss']
 })
-export class ModalInfoComponent {
+export class ModalInfoComponent implements OnInit {
+  services: Service[] = [];
+  barbeariaId: string;
 
-  constructor(@Inject(MAT_DIALOG_DATA) public data: Barbeiros, private dialogRef: MatDialogRef<ModalInfoComponent>) {}
+  constructor(
+    @Inject(MAT_DIALOG_DATA) public data: { barber: Barbeiros, barbeariaId: string },
+    private dialogRef: MatDialogRef<ModalInfoComponent>,
+    private firestore: Firestore
+  ) {
+    this.barbeariaId = data.barbeariaId;
+  }
+
+  ngOnInit(): void {
+    this.loadServices();
+  }
+
+  async loadServices() {
+    try {
+      const servicesCollection = collection(this.firestore, `barbearia/${this.barbeariaId}/barbers/${this.data.barber.id}/services`);
+      const querySnapshot = await getDocs(servicesCollection);
+      this.services = querySnapshot.docs.map(doc => ({
+        id: doc.id,
+        ...doc.data()
+      })) as Service[];
+    } catch (error) {
+      console.error('Erro ao carregar serviços:', error);
+    }
+  }
 
   close(): void {
     this.dialogRef.close();
