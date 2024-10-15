c19e42e58d413d928c58ddd5089224dff6daca02
joaovs19 <joaovitorsa20@gmail.com>
2024-10-14 21:14:47 -0300

    files for styling

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-header',
@@ -8,5 +8,41 @@ import { Component } from '@angular/core';
   styleUrl: './header.component.scss'
 })
 export class HeaderComponent {
+  dropdownOpen: boolean = false;
+  router: any;
 
+  toggleDropdown() {
+    this.dropdownOpen = !this.dropdownOpen;
+  }
+
+   // Navegar para a página de editar perfil
+   goToEditProfile() {
+    this.dropdownOpen = false;  // Fechar o dropdown ao navegar
+    this.router.navigate(['/edit-profile']).then(() => {
+      console.log('Navigated to Edit Profile');
+    });
+  }
+
+  // Navegar para a página de barbearias favoritas
+  goToFavorites() {
+    this.dropdownOpen = false;  // Fechar o dropdown ao navegar
+    this.router.navigate(['/favorites']).then(() => {
+      console.log('Navigated to Favorites');
+    });
+  }
+
+  logout() {
+    this.dropdownOpen = false;  // Fechar o dropdown
+    // Implementar lógica de logout aqui
+    console.log('Usuário deslogado');
+  }
+
+  // Fechar o dropdown se clicar fora
+  @HostListener('document:click', ['$event'])
+  onClickOutside(event: MouseEvent) {
+    const target = event.target as HTMLElement;
+    if (!target.closest('.profile')) {
+      this.dropdownOpen = false;
+    }
+  }
 }
