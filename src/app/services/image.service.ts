487e883ce7f0f8327b24ab966df9ab3011c27afe
Guilherme Berssanette <guilherme.berssanette@gmail.com>
2024-10-14 21:00:16 -0300

    add image service

---
+++ src/app/services/image.service.ts
@@ -0,0 +1,29 @@
+import { Injectable } from '@angular/core';
+import { getStorage, ref, listAll, getDownloadURL, getMetadata } from '@angular/fire/storage';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ImageService {
+  private storage = getStorage();
+
+  async getGalleryItems(barberId: string): Promise<{ imageUrl: string, comment: string, filePath: string }[]> {
+    try {
+      const galleryRef = ref(this.storage, `gallery/${barberId}`);
+      const gallerySnapshot = await listAll(galleryRef);
+
+      return await Promise.all(
+        gallerySnapshot.items.map(async (item) => {
+          const imageUrl = await getDownloadURL(item);
+          const metadata = await getMetadata(item);
+          const comment = metadata.customMetadata?.['comment'] || 'Sem comentário';
+          const filePath = item.fullPath;
+          return { imageUrl, comment, filePath };
+        })
+      );
+    } catch (error) {
+      console.error('Erro ao carregar a galeria de imagens:', error);
+      return [];
+    }
+  }
+}
