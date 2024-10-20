d2f30ce6ae322618b77549d87ef35cb46ba26c03
Guilherme Berssanette <guilherme.berssanette@gmail.com>
2024-10-19 22:47:55 -0300

    add function logout

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,46 +1,51 @@
 import { Router, RouterLink } from '@angular/router';
 import { Component, HostListener } from '@angular/core';
 import { RegisterComponent } from '../../pages/user/register/register.component';
-
+import { Auth, signOut } from '@angular/fire/auth';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [ RegisterComponent, RouterLink ],
+  imports: [RegisterComponent, RouterLink],
   templateUrl: './header.component.html',
-  styleUrl: './header.component.scss'
+  styleUrls: ['./header.component.scss']
 })
 export class HeaderComponent {
   dropdownOpen: boolean = false;
-  router: any;
+
+  constructor(private auth: Auth, private router: Router) {}
 
   toggleDropdown() {
     this.dropdownOpen = !this.dropdownOpen;
   }
 
-   // Navegar para a página de editar perfil
-   goToEditProfile() {
-    this.dropdownOpen = false;  // Fechar o dropdown ao navegar
+  goToEditProfile() {
+    this.dropdownOpen = false;
     this.router.navigate(['/edit-profile']).then(() => {
       console.log('Navigated to Edit Profile');
     });
   }
 
-  // Navegar para a página de barbearias favoritas
   goToFavorites() {
-    this.dropdownOpen = false;  // Fechar o dropdown ao navegar
+    this.dropdownOpen = false;
     this.router.navigate(['/favorites']).then(() => {
       console.log('Navigated to Favorites');
     });
   }
 
-  logout() {
-    this.dropdownOpen = false;  // Fechar o dropdown
-    // Implementar lógica de logout aqui
-    console.log('Usuário deslogado');
+  async logout() {
+    this.dropdownOpen = false;
+    try {
+      await signOut(this.auth);
+      console.log('Usuário deslogado');
+      await this.router.navigate(['/']);
+      console.log('Navegação para a página inicial concluída');
+    } catch (error) {
+      console.error('Erro ao fazer logout ou navegar:', error);
+    }
   }
 
-  // Fechar o dropdown se clicar fora
+
   @HostListener('document:click', ['$event'])
   onClickOutside(event: MouseEvent) {
     const target = event.target as HTMLElement;
