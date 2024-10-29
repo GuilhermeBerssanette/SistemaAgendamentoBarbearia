7292102899b3d9823cc2146cbfbb8c52b45eefe6
Guilherme Berssanette <guilherme.berssanette@gmail.com>
2024-10-29 08:49:25 -0300

    add modal recovery password

---
+++ src/app/pages/user/login/modals/modal-recovery-password/modal-recovery-password.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-modal-recovery-password',
+  standalone: true,
+  imports: [],
+  templateUrl: './modal-recovery-password.component.html',
+  styleUrl: './modal-recovery-password.component.scss'
+})
+export class ModalRecoveryPasswordComponent {
+
+}
