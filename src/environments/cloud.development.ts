8f0aec8688ccbc043d0575aa2af6e7c887869bca
Guilherme Berssanette <guilherme.berssanette@gmail.com>
2024-12-05 20:21:28 -0300

    many implementations

---
+++ src/environments/cloud.development.ts
@@ -0,0 +1,7 @@
+export const cloud = {
+  CloudConfig: {
+    clientId: '228837673406-hi9740ollb29cclqatptb2ni4mrg9cv3.apps.googleusercontent.com',
+    clientSecret: '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',
+    scopes: 'https://www.googleapis.com/auth/calendar https://www.googleapis.com/auth/calendar.events',
+  }
+};
