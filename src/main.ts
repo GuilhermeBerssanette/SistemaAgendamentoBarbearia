23406669519ecbe9b7b8f41ac3143887f2ea0ebe
joaovs19 <joaovitorsa20@gmail.com>
2024-12-10 03:13:36 -0300

    main

--- src/main.ts
+++ src/main.ts
@@ -8,6 +8,6 @@ bootstrapApplication(AppComponent, appConfig)
     const splashScreen = document.getElementById('splash-screen');
     if (splashScreen) {
       splashScreen.style.opacity = '0';
-      setTimeout(() => splashScreen.remove(), 1000);
+      setTimeout(() => splashScreen.remove(), 3500);
     }
   });
